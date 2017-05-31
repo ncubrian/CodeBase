b6722b07fb8f06f00525c469c5bff5facec69f45
ncubrian <ncubrian@users.noreply.github.com>
2017-05-31 14:39:24 +0800

    Create etcd.sh

---
+++ shell/etcd.sh
@@ -0,0 +1,5 @@
+curl http://127.0.0.1:2379/v2/members
+etcdctl member list # 成员列表
+curl http://127.0.0.1:2379/v2/stats/leader # 查看leader信息
+etcdctl --endpoints http://127.0.0.1:2379  --user root user add admin # 添加一个非特权账号
+etcdctl --endpoints http://127.0.0.1:2379  --user root:xxx role grant-permission admin readwrite --prefix /etcd3/foo # 给admin角色添加权限
