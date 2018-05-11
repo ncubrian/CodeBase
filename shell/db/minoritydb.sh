1ad0d210a8df1cae9106c9403c3c08123a70525a
ncubrian <ncubrian@users.noreply.github.com>
2018-05-11 15:40:52 +0800

    Update minoritydb.sh

--- shell/db/minoritydb.sh
+++ shell/db/minoritydb.sh
@@ -1,2 +1,3 @@
 influxdb
 /usr/bin/influxd -pidfile /var/run/influxdb/influxd.pid -config /etc/influxdb/influxdb.conf
+curl "http://127.0.0.1:8086/query" -d "db=foo" -d "chunked=true" -d "q=select * from bar" # export data from influxdb as json format
