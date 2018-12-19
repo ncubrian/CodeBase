0c2d4927ff7e49c2f2a8f50aac12ab40b41ba6cc
ncubrian <ncubrian@users.noreply.github.com>
2018-12-19 23:21:15 +0800

    fix bugs

--- Golang/longestMatch.go
+++ Golang/longestMatch.go
@@ -1,70 +1,59 @@
 package main
 
 import (
-	"strings"
 	"fmt"
 )
 
-func isNextTo(prev, curr []int) bool {
+type Position struct {
+	p int
+	consecutive int
+}
+
+// 比较当前字符的所有位置和前一字符的所有位置，查找是否有相邻的，如果有则那个位置的连续值较前一个的加1
+// 返回最长的连续值所处的位置
+func isNextTo(prev, curr []*Position, longest *Position) *Position {
 	for _, p := range curr {
 		for _, q := range prev {
-			if (q + 1) == p {
-				return true
+			if (q.p + 1) == p.p {
+				p.consecutive = q.consecutive + 1
+				if longest == nil || p.consecutive > longest.consecutive {
+					longest = p
+				}
 			}
 		}
 	}
-	return false
+	return longest
 }
 
-// 找出s1和s2中最长的相同的字符串，如果有多个则返回第一个
+// 找出s1和s2中最长的相同字符串，如果有多个则返回第一个
 func stringSimilarity(s1, s2 string) string {
-	s1 = strings.ToLower(s1)
-
 	// to lower case and decomposite
-	m2 := make(map[rune][]int)
-	lowers2 := make([]rune, len(s2))
+	m2 := make(map[rune][]*Position)
 	for i, r := range s2 {
 		if 'A' <= r && r <= 'Z' {
 			r += 'a' - 'A'
 		}
-		lowers2[i] = r
-		m2[r] = append(m2[r], i)
-	}
-	fmt.Printf("m2: %+v\n", m2)
-	s2 = string(lowers2)
 
-	var charArr, longest []rune
-	var prevPos []int
+		m2[r] = append(m2[r], &Position{i, 0})
+	}
+
+	var prevPos []*Position
+	var longest *Position
 	for _, r := range s1 {
+		if 'A' <= r && r <= 'Z' {
+			r += 'a' - 'A'
+		}
 		if currPos, ok := m2[r]; ok {
 			if prevPos == nil {
 				prevPos = currPos
-				charArr = append(charArr, r)
 			} else {
-				// s1中相邻但s2中没有相邻的，则认为断开了，从此处重新开始查找
-				if !isNextTo(prevPos, currPos) {
-					if len(charArr) > len(longest) {
-						longest = charArr
-					}
-					charArr = nil
-				}
-				charArr = append(charArr, r)
+				longest = isNextTo(prevPos, currPos, longest)
 				prevPos = currPos
 			}
-		} else {
-			// 如果s1中的一个字符在s2中没有，则认为断开了，从此处重新开始查找
-			if len(charArr) > len(longest) {
-				longest = charArr
-			}
-			charArr = nil
 		}
 	}
 
-	if len(charArr) > len(longest) {
-		longest = charArr
-	}
-
-	return string(longest)
+	return string(s2[longest.p - longest.consecutive : longest.p + 1])
 }
 
 func main() {
