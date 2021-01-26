a164fac4b452b5efacd54a69989abe96054aa112
ncubrian <ncubrian@users.noreply.github.com>
2021-01-26 14:38:04 +0800

    Update minoritydb.sh

--- shell/db/minoritydb.sh
+++ shell/db/minoritydb.sh
@@ -1,3 +1,5 @@
 influxdb
 /usr/bin/influxd -pidfile /var/run/influxdb/influxd.pid -config /etc/influxdb/influxdb.conf
 curl "http://127.0.0.1:8086/query" -d "db=foo" -d "chunked=true" -d "q=select * from bar" # export data from influxdb as json format
+./etcdctl --endpoints="localhost:2379" get / --keys-only --prefix
+./etcdctl --endpoints="localhost:2379" put /foobar "voodoo"
